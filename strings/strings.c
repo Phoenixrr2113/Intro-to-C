89f22d5869e849ecfe55af65bfbf3e38de7950a6
Randy Wilson <phoenixrr2113@gmail.com>
2019-06-13 17:05:21 -0400

    finished part of string

--- strings/strings.c
+++ strings/strings.c
@@ -9,8 +9,35 @@
 */
 int string_length(char *s)
 {
-
+    int i = 0;
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+    // for (i; i != '\0'; i++)
+    // {
+    //     if (s[i] == '\0')
+    //     {
+    //         return count;
+    //     }
+    //     else
+    //     {
+    //         count++;
+    //     }
+    // }
+    return i;
 }
+
+// int array[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+// int sum = 0;
+// int i;
+
+// for (i = 0; i < 10; i++) {
+//     sum += array[i];
+// }
+
+// /* sum now contains a[0] + a[1] + ... + a[9] */
+// printf("Sum of the array is %d\n", sum);
 
 /*
     Write a function that reverses the order of string s and outputs 
@@ -20,7 +47,6 @@ int string_length(char *s)
 */
 char *reverse_string(char *rv, char *s)
 {
-
 }
 
 #ifndef TESTING
@@ -37,4 +63,3 @@ int main(void)
     return 0;
 }
 #endif
-    
